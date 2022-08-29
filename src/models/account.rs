c323fae171521b2ddf77c1e241eec75faf8ee64d
Sebastian Sastre <sebastian.sastre@telna.com>
2022-08-29 12:02:58 -0300

    dispute_increase_disputed_balance_and_maintain_total green

--- src/models/account.rs
+++ src/models/account.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, cell::RefMut};
+use std::{cell::RefMut, collections::HashMap};
 
-use crate::models::transactions::transactions_set;
+use crate::models::transactions::{transactions_set, TRANSACTIONS};
 
 use super::transaction::{Amount, ClientID, Transaction};
 
@@ -31,35 +31,53 @@ impl Account {
 
     // A deposit is a credit to the client's asset account, meaning it should increase the available and total funds of the client account.
     pub fn process_deposit(&mut self, transaction: &Transaction) -> Result<Transaction> {
-      println!("Processing DEPOSIT {:?}", transaction);
-      self.available += transaction.amount;
-      Ok(transaction.clone())
+        println!("Processing DEPOSIT {:?}", transaction);
+        self.available += transaction.amount;
+        Ok(transaction.clone())
     }
 
     // A withdraw is a debit to the client's asset account, meaning it should decrease the available and total funds of the client account.
     pub fn process_withdrawal(&mut self, transaction: &Transaction) -> Result<Transaction> {
-      println!("Processing WITHDRAWAL {:?}", transaction);
-      if self.available > transaction.amount {
-        self.available -= transaction.amount;
-        Ok(transaction.clone())
-      } else {
-        Err(RejectedTransaction)
-      }
+        println!("Processing WITHDRAWAL {:?}", transaction);
+        if self.available > transaction.amount {
+            self.available -= transaction.amount;
+            Ok(transaction.clone())
+        } else {
+            Err(RejectedTransaction)
+        }
     }
 
-    // A dispute represents a client's claim that a transaction was erroneous and should be reversed. 
-    // The transaction shouldn't be reversed yet but the associated funds should be held. 
-    // This means that the clients available funds should decrease by the amount disputed, 
-    // their held funds should increase by the amount disputed, 
+    // A dispute represents a client's claim that a transaction was erroneous and should be reversed.
+    // The transaction shouldn't be reversed yet but the associated funds should be held.
+    // This means that the clients available funds should decrease by the amount disputed,
+    // their held funds should increase by the amount disputed,
     // while their total funds should remain the same.
     pub fn process_dispute(&mut self, transaction: &Transaction) -> Result<Transaction> {
-      println!("Processing DISPUTE {:?}", transaction);
-      if self.available > transaction.amount {
-        self.available -= transaction.amount;
-        Ok(transaction.clone())
-      } else {
-        Err(RejectedTransaction)
-      }
+        println!("Processing DISPUTE {:?}", transaction);
+        let transactions = TRANSACTIONS
+            .read()
+            .expect("Could not get read access to the transactions store");
+        let disputed_tx = transactions.get(transaction.id);
+        match disputed_tx {
+            None => {
+                println!(
+                    "Ignoring invalid disputed transaction ID {:?}",
+                    transaction.id
+                );
+                Err(RejectedTransaction)
+            }
+            Some(tx) => {
+                // Ok, but what the process should do  with a dispute that is greater than the available balance?
+                // Until other clarification, I'm coding it to reject that claim.
+                if self.available > tx.amount {
+                  self.held += tx.amount;
+                  self.available -= tx.amount;
+                  Ok(transaction.clone())
+                } else {
+                    Err(RejectedTransaction)
+                }
+            }
+        }
     }
 
     pub fn available_balance(&self) -> Amount {
@@ -70,4 +88,3 @@ impl Account {
         self.available + self.held
     }
 }
-// 
